053aab4ea61a9047131194ba96950297809fe36f
APIVerve Team <hello@apiverve.com>
2025-02-03 15:28:52 -0600

    Minor Version Update

---
+++ CountryCallingCodeQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class CountryCallingCodeQueryOptions {
+public string name { get; set; }
+}
+
+}
